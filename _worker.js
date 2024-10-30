8a3982c4d0f6c302a205e0ecd97afa24beadf0f6
onepunchko <ylily7771@gmail.com>
2024-10-30 04:45:35 -0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,24 +3,27 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
-  // 目标 URL
+  // Target URL
   const targetUrl = 'https://raw.githubusercontent.com/onepunchko/chromego_merge/refs/heads/main/sub/merged_proxies_new.yaml'
 
-  // 创建新的请求，使用目标 URL 和原始请求的 HTTP 方法和头部
+  // Create a new request with the target URL and original request's method and headers
   const newRequest = new Request(targetUrl, {
     method: request.method,
     headers: request.headers,
-    body: request.method === 'POST' ? request.body : null,
+    body: request.method === 'POST'? request.body : null,
     redirect: 'follow'
   })
 
-  // 发送请求到目标服务器
+  // Send the request to the target server
   const response = await fetch(newRequest)
 
-  // 返回目标服务器的响应
+  // Return the target server's response
   return new Response(response.body, {
     status: response.status,
     statusText: response.statusText,
     headers: response.headers
   })
 }
+
+// Add this line to make it compatible with Pages
+export default handleRequest
