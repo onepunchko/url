71ba3f853b042cd7c05a5ae664543e251cc1eaa2
onepunchko <ylily7771@gmail.com>
2024-10-30 04:52:21 -0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,41 +1,17 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
 export default {
   async fetch(request) {
     // 目标 URL
     const targetUrl = 'https://raw.githubusercontent.com/onepunchko/chromego_merge/refs/heads/main/sub/merged_proxies_new.yaml';
-
-async function handleRequest(request) {
-  // 目标 URL
-  const targetUrl = 'https://raw.githubusercontent.com/onepunchko/chromego_merge/refs/heads/main/sub/merged_proxies_new.yaml'
-    // 创建新的请求，使用目标 URL 和原始请求的 HTTP 方法和头部
+        // 创建新的请求，使用目标 URL 和原始请求的 HTTP 方法和头部
     const newRequest = new Request(targetUrl, {
       method: request.method,
       headers: request.headers,
       body: request.method === 'POST' ? request.body : null,
       redirect: 'follow'
     });
-
-  // 创建新的请求，使用目标 URL 和原始请求的 HTTP 方法和头部
-  const newRequest = new Request(targetUrl, {
-    method: request.method,
-    headers: request.headers,
-    body: request.method === 'POST' ? request.body : null,
-    redirect: 'follow'
-  })
-    // 发送请求到目标服务器
+        // 发送请求到目标服务器
     const response = await fetch(newRequest);
-
-  // 发送请求到目标服务器
-  const response = await fetch(newRequest)
-  // 返回目标服务器的响应
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: response.headers
-  })
-    // 返回目标服务器的响应
+        // 返回目标服务器的响应
     return new Response(response.body, {
       status: response.status,
       statusText: response.statusText,
